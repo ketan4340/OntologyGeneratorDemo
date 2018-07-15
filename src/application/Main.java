f5b2fddbace68a92a4baa3ee77be5ed23d26b959
ketan4340 <ken_tanabe915@outlook.jp>
2018-07-15 16:28:17 +0900

    deleted parameters from the WholePanel's constructor.

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,14 +1,6 @@
 package application;
 	
 import demo.WholePanel;
-import demo.controller.BuildOntologyController;
-import demo.controller.UseOntologyController;
-import demo.ontology.OntologyModel;
-import demo.ontology.OntologyTableView;
-import demo.textField.DocumentModel;
-import demo.textField.DocumentView;
-import demo.textField.InputTextModel;
-import demo.textField.InputTextView;
 import javafx.application.Application;
 import javafx.embed.swing.SwingNode;
 import javafx.scene.Scene;
@@ -26,7 +18,7 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			final SwingNode swingNode = new SwingNode();
-	        swingNode.setContent(createWholePanel());
+	        swingNode.setContent(new WholePanel());
 
 	        StackPane pane = new StackPane();
 	        pane.getChildren().add(swingNode);
@@ -43,30 +35,4 @@ public class Main extends Application {
 		}
 	}
 	
-	/*
-	private void createSwingContent(SwingNode swingNode) {
-	     SwingUtilities.invokeLater(() -> {
-	    	   swingNode.setContent(createWholePanel());
-	        });
-	}
-	*/
-
-	private WholePanel createWholePanel() {
-		/** Model生成 **/
-		InputTextModel iptModel = new InputTextModel();
-		OntologyModel ontModel = new OntologyModel();
-		DocumentModel docModel = new DocumentModel();
-		OntologyModel subOntModel = new OntologyModel();
-
-		/** Controller生成 **/
-		BuildOntologyController buildCtrl = new BuildOntologyController(iptModel, ontModel);
-		UseOntologyController useCtrl = new UseOntologyController(docModel, ontModel, subOntModel);
-
-		/** View生成 **/
-		InputTextView iptView = new InputTextView(buildCtrl);
-		OntologyTableView ontView = new OntologyTableView(useCtrl, buildCtrl);
-		DocumentView docView = new DocumentView(useCtrl);
-		
-		return new WholePanel(iptView, ontView, docView);
-	}
 }
--- src/demo/WholePanel.java
+++ src/demo/WholePanel.java
@@ -4,15 +4,35 @@ import java.awt.GridLayout;
 
 import javax.swing.JPanel;
 
+import demo.controller.BuildOntologyController;
+import demo.controller.UseOntologyController;
+import demo.ontology.OntologyModel;
 import demo.ontology.OntologyTableView;
+import demo.textField.DocumentModel;
 import demo.textField.DocumentView;
+import demo.textField.InputTextModel;
 import demo.textField.InputTextView;
 
 public class WholePanel extends JPanel {
 	private static final long serialVersionUID = -8108903772139391920L;
 
 	
-	public WholePanel(InputTextView iptView, OntologyTableView ontView, DocumentView docView) {	
+	public WholePanel() {
+		/** Model生成 **/
+		InputTextModel iptModel = new InputTextModel();
+		OntologyModel ontModel = new OntologyModel();
+		DocumentModel docModel = new DocumentModel();
+		OntologyModel subOntModel = new OntologyModel();
+
+		/** Controller生成 **/
+		BuildOntologyController buildCtrl = new BuildOntologyController(iptModel, ontModel);
+		UseOntologyController useCtrl = new UseOntologyController(docModel, ontModel, subOntModel);
+
+		/** View生成 **/
+		InputTextView iptView = new InputTextView(buildCtrl);
+		OntologyTableView ontView = new OntologyTableView(useCtrl, buildCtrl);
+		DocumentView docView = new DocumentView(useCtrl);
+		
 		
 		setLayout(new GridLayout(1, 3));				// 横に3つ並べる
 		// 子ビュー(JPanel)を配置
@@ -20,4 +40,5 @@ public class WholePanel extends JPanel {
 		add(ontView);
 		add(docView);
 	}
+	
 }
